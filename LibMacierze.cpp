c615c4c54190c2affad71158fb877101d71eba0f
Swałdek Krzysztof <35688726+FilipCysEk@users.noreply.github.com>
2018-05-04 19:32:26 +0200

    Add files via upload

---
+++ LibMacierze.cpp
@@ -0,0 +1,243 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+void printMacierz(double *mac, int size){
+	for(int i = 0; i < size; i++){
+		//cout<<"";
+		for(int j = 0; j < size; j++){
+			cout << mac[size*i+j] << "\t";
+		}
+		cout << endl;
+	}
+}
+
+void printMacierz(double **mac, int size){
+	cout << setprecision(4) << fixed;
+	for(int i = 0; i < size; i++){
+		for(int j = 0; j < size; j++){
+			cout << setw(11) << mac[i][j];
+		}
+		cout << endl;
+	}
+}
+
+void printMacierz2(double **mac, int size){
+	cout << "{";
+	for(int i = 0; i < size; i++){
+		cout<<"{";
+		cout << mac[i][0];
+		for(int j = 1; j < size; j++){
+			
+			cout << ", " <<mac[i][j];
+		}
+		cout << "},"<< endl;
+	}
+	cout << "}" << endl <<endl;
+}
+
+void printVector(double *vect, int size){
+	cout << setprecision(4) << fixed;
+	for(int i = 0; i < size; i++){
+		cout<< setw(11) << vect[i];
+	}
+}
+
+
+void printVectorPrecission(double *vect, int size, int precision){
+	cout << setprecision(precision);
+	for(int i = 0; i < size; i++){
+		cout<< setw(precision + 5) << vect[i];
+	}
+}
+
+void allocating(double **mac, int size){
+	for(int i = 0; i < size; i++)
+		mac[i] = new double[size];
+}
+
+void deleting(double **mac, int size){
+	for(int i = 0; i < size; i++)
+		delete(mac[i]);
+		
+	delete(mac);
+}
+
+void copyMacierz(double **from, double **to, int size){
+	for(int i = 0; i < size; i++){
+		for(int j = 0; j < size; j++){
+			to[i][j] = from[i][j];
+		}
+	}
+}
+
+void copyMacierz(double *from, double *to, int size){
+	for(int i = 0; i < size; i++){
+		to[i] = from[i];
+	}
+}
+
+void zerowanie(double **mac, int size){
+	for(int i = 0; i < size; i++){
+		for(int j = 0; j < size; j++)
+			mac[i][j] = 0;
+	}
+}
+
+void checking(double **A, double *x, double *b, double *w, int size){
+	for(int i = 0; i < size; i++){
+		w[i] = 0;
+		for(int j = 0; j < size; j++){
+			w[i] += A[i][j] * x[j];
+		}
+		w[i] = w[i] - b[i];
+	}
+	
+}
+
+//---------------------------------------------------------------------
+double det(double **mac, int size){
+	if(size == 1){
+		return mac[0][0];
+	}
+	if(size == 2){
+		return mac[0][0] * mac[1][1] - mac[0][1] * mac[1][0];
+	}
+	if(size == 3){
+		double det = 0, temp = 1, temp1 = 1;
+		for(int i = 0; i < size; i++){
+			for(int j = 0; j < size; j++){
+				temp *= mac[(i + j) % size][j];
+				temp1 *= mac[(j + i) % size][size - 1 - j];
+			}
+			det += temp;
+			det -= temp1; 
+			temp = 1;
+			temp1 = 1;
+		}
+		return det;
+	}
+	cout << "ERROR nie potrafiê obliczyæ wyznacznika" << endl;
+	exit(4);
+}
+
+double detTrojkatna(double **mac, int size){
+	double det = 1;
+	for(int i = 0; i < size; i++){
+		det *= mac[i][i];
+	}
+	return det;
+}
+
+void macierzOdwrotna(double **mac, int size, double **odwrotna, double detMac){
+	double **tempMac = new double *[size-1];
+	allocating(tempMac, size-1);
+	
+	//iteratory macierzy tymczasowej
+	int ti = 0, tj = 0;
+	//Eliminacja linii nie liczonych
+	for(int i = 0; i < size; i++){
+		for(int j = 0; j < size; j++){
+			//liczenie linii
+			for(int n = 0; n < size; n++){
+				if(n != i){
+					for(int m = 0; m < size; m++){
+						if(m != j){
+							tempMac[ti][tj] = mac[n][m];
+							tj++;
+							if(tj > size -1){ cout<< "tj out of range" <<endl; exit(2);}
+						}
+					}
+					tj = 0;
+					ti++;
+					if(tj > size -1){ cout<< "ti out of range" <<endl; exit(2);}
+				}
+			}
+			odwrotna[j][i] =  (1/detMac)*det(tempMac, size-1);
+			if((i + j) % 2 == 1)
+				odwrotna[j][i] *= -1;
+			ti = 0;
+		}
+	}
+	deleting(tempMac, size-1);
+	
+}
+
+void macierzDiagonalnaOdwrotna(double **A, double **w, int size){
+	zerowanie(w, size);
+	for(int i = 0; i < size; i++){
+		w[i][i] = 1/A[i][i];
+	}
+}
+
+void mnozenieMacierzy(double **A, double **B, double **w, int size){
+	for(int iw = 0; iw < size; iw++){
+		for(int jw = 0; jw < size; jw++){
+			w[iw][jw] = 0;
+			for(int i = 0; i < size; i++){
+				w[iw][jw] += A[iw][i] * B[i][jw];
+			}
+		}
+	}
+}
+
+void mnozenieMacierzy(double **A, double *B, double *w, int size){
+	for(int iw = 0; iw < size; iw++){
+		w[iw] = 0;
+		for(int i = 0; i < size; i++){
+			w[iw] += A[iw][i] * B[i];
+		}
+	}
+}
+
+void mnozenieMacierzy(double **A, double b, double **w, int size){
+	for(int i = 0; i < size; i++){
+		for(int j = 0; j < size; j++){
+			w[i][j] = A[i][j] * b;
+		}
+	}
+}
+
+void mnozenieMacierzy(double *A, double b, double *w, int size){
+	for(int i = 0; i < size; i++){
+		w[i] = A[i] * b;
+	}
+}
+
+void dodawanieMacierzy(double **A, double **B, double **w, int size){
+	for(int i = 0; i < size; i++){
+		for(int j = 0; j < size; j++)
+			w[i][j] = A[i][j] + B[i][j];
+	}
+}
+
+void dodawanieMacierzy(double *A, double *B, double *w, int size){
+	for(int i = 0; i < size; i++)
+		w[i] = A[i] + B[i];
+}
+
+void macierzTrojkatnaDolnaZX(double **L, double *x, double*b, int size)
+{
+	x[0] = b[0]/L[0][0];
+	for(int i = 1; i < size; i++){
+		x[i] = b[i];
+		for(int j = 0; j < i; j++){
+			x[i] -= x[j]*L[i][j];
+		}
+		x[i] /= L[i][i];
+	}
+}
+
+void macierzTrojkatnaGornaZX(double **L, double *x, double*b, int size)
+{
+	x[size - 1] = b[size - 1]/L[size - 1][size - 1];
+	for(int i = size - 2; i >= 0; i--){
+		x[i] = b[i];
+		for(int j = size - 1; j > i; j--){
+			x[i] -= x[j]*L[i][j];
+		}
+		x[i] /= L[i][i];
+	}
+}
---
+++ lab7.cpp
@@ -0,0 +1,361 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+#include "LibMacierze.cpp"
+
+using namespace std;
+
+//Zmienne globalne 
+double static nOperations = 500;
+double static minStepSize = 0.000001;
+double static deviation = 0.0001;
+
+void insertDefaultVal(double **mac, double *vect, double *x0){
+	mac[0][0] = 100;
+	mac[0][1] = 1;
+	mac[0][2] = -2;
+	mac[0][3] = 3;
+	mac[1][0] = 4;
+	mac[1][1] = 300;
+	mac[1][2] = -5;
+	mac[1][3] = 6;
+	mac[2][0] = 7;
+	mac[2][1] = -8;
+	mac[2][2] = 400;
+	mac[2][3] = 9;
+	mac[3][0] = -10;
+	mac[3][1] = 11;
+	mac[3][2] = -12;
+	mac[3][3] = 200;
+	
+	vect[0] = 395;
+	vect[1] = 603;
+	vect[2] = -415;
+	vect[3] = -606;
+	
+	for(int i = 0; i < 4; i++)
+		x0[i]=1;
+}
+
+void podzialNaLDU(double **A, double **L, double **D, double **U, int size){
+	zerowanie(L, size);
+	zerowanie(D, size);
+	zerowanie(U, size);
+	
+	for(int i = 0; i < size; i++){
+		for(int j = 0; j < size; j++){
+			if(j < i){
+				L[i][j] = A[i][j];
+			}
+			else if(i == j){
+				D[i][j] = A[i][j];
+			}
+			else
+				U[i][j] = A[i][j];
+		}
+	}
+}
+
+double maxKrokSize(double *A, double *B, int size){
+	double max = fabs(A[0] - B[0]);
+	
+	for(int i = 1; i < size; i++)
+		if(max < fabs(A[i] - B[i]))
+			max = fabs(A[i] - B[i]);
+			
+	return max;
+}
+
+double maxDeviation(double **A, double *x, double *b, int size){
+	double *temp = new double [size];
+	mnozenieMacierzy(A, x, temp, size);
+	double max = fabs(temp[0] - b[0]);
+	
+	for(int i = 1; i < size; i++)
+		if(temp[i] > max)
+			max = fabs(temp[i] - b[i]);
+	
+	delete(temp);
+	return max;
+}
+
+void metodaJacobiego(double **A, double **L, double **D, double **U, double *x, double *x0, double *b, int size){
+	double *xm1 = new double [size];
+	copyMacierz(x0, xm1, size);
+	
+	double **M = new double *[size];
+	allocating(M, size);
+	
+	double **Dodwr = new double *[size];
+	allocating(Dodwr, size);
+	
+	double **Temp = new double *[size];
+	allocating(Temp, size);
+	
+	double *C = new double [size];
+	
+	macierzDiagonalnaOdwrotna(D, Dodwr, size);
+	
+	dodawanieMacierzy(L, U, Temp, size);
+	mnozenieMacierzy(Dodwr, Temp, M, size);
+	mnozenieMacierzy(M, -1, M, size);
+	
+	mnozenieMacierzy(Dodwr, b, C, size);
+	
+	bool endByOperations = 1;
+	
+	double estymator, residium;
+	
+	for(int i = 0; i < nOperations; i++){
+		mnozenieMacierzy(M, xm1, x, size);
+		//mnozenieMacierzy(x, -1, x, size);
+		dodawanieMacierzy(x, C, x, size);
+		
+		//Sprawdzanie, czy wielkoœæ kroku jet odpowiednia
+		estymator = maxKrokSize(x, xm1, size);
+		residium = maxDeviation(A, x, b, size);
+		
+		cout<<"Iteracja " << i << endl;
+		printVector(x, size);
+		cout<< endl << "Estymator: " << estymator << "\t\tResidium: " << residium << endl << endl;
+		
+		if(estymator < minStepSize){
+			endByOperations = 0;
+			cout << "Zakonczono, poniewaz krok byl zbyt maly!!" << endl;
+			break;
+		}
+		
+		if(residium < deviation){
+			endByOperations = 0;
+			cout << "Zakonczono, poniewaz znalezlismy sie zbyt blisko wyniku!!" << endl;
+			break;
+		}
+			
+		for(int j = 0; j < size; j++)
+			xm1[j] = x[j];
+	}
+	
+	if(endByOperations)
+		cout << "Zakonczono, poniewaz przekroczono ilosc operacji!!" << endl;
+	
+	deleting(M, size);
+	deleting(Dodwr, size);
+	deleting(Temp, size);
+	delete(xm1);
+}
+
+void metodaGaussaSeidela(double **A, double **L, double **U, double **D, double *x0, double *x, double *b, int size){
+	double *xm1 = new double [size];
+	copyMacierz(x0, xm1, size);
+	
+	double **M = new double *[size];
+	allocating(M, size);
+	
+	double **Dodwr = new double *[size];
+	allocating(Dodwr, size);
+	
+	double **Temp = new double *[size];
+	allocating(Temp, size);
+	
+	double *C = new double [size];
+	
+	dodawanieMacierzy(L, D, Temp, size);
+	
+	double det_val = detTrojkatna(Temp, size);
+	macierzOdwrotna(Temp, size, M, det_val);
+	
+	mnozenieMacierzy(M, b, C, size);
+	
+	mnozenieMacierzy(M, U, Temp, size);
+	mnozenieMacierzy(Temp, -1, M, size);
+	
+	bool endByOperations = 1;
+	
+	double estymator, residium;
+	
+	for(int i = 0; i < nOperations; i++){
+		mnozenieMacierzy(M, xm1, x, size);
+		dodawanieMacierzy(x, C, x, size);
+		
+		//Sprawdzanie, czy wielkoœæ kroku jet odpowiednia
+		estymator = maxKrokSize(x, xm1, size);
+		residium = maxDeviation(A, x, b, size);
+		
+		cout<<"Iteracja " << i << endl;
+		printVector(x, size);
+		cout<< endl << "Estymator: " << estymator << "\t\tResidium: " << residium << endl << endl;
+		
+		if(estymator < minStepSize){
+			endByOperations = 0;
+			cout << "Zakonczono, poniewaz krok byl zbyt maly!!" << endl;
+			break;
+		}
+		
+		if(residium < deviation){
+			endByOperations = 0;
+			cout << "Zakonczono, poniewaz znalezlismy sie zbyt blisko wyniku!!" << endl;
+			break;
+		}
+			
+		for(int j = 0; j < size; j++)
+			xm1[j] = x[j];
+	}
+	
+	if(endByOperations)
+		cout << "Zakonczono, poniewaz przekroczono ilosc operacji!!" << endl;
+		
+	deleting(M, size);
+	deleting(Dodwr, size);
+	deleting(Temp, size);
+	delete(xm1);
+}
+
+void metodaSOR(double **A, double **L, double **U, double **D, double *x0, double *x, double *b, double w, int size){
+	double *xm1 = new double [size];
+	copyMacierz(x0, xm1, size);
+	
+	double **M = new double *[size];
+	allocating(M, size);
+	
+	double **Dodwr = new double *[size];
+	allocating(Dodwr, size);
+	
+	double **Temp = new double *[size];
+	allocating(Temp, size);
+	
+	double **Temp1 = new double *[size];
+	allocating(Temp1, size);
+	
+	double *C = new double [size];
+	
+	double det_val, Temp_val;
+	
+	mnozenieMacierzy(D, 1./w, Temp, size);
+	dodawanieMacierzy(L, Temp, Temp, size);
+	
+	det_val = detTrojkatna(Temp, size);
+	macierzOdwrotna(Temp, size, M, det_val);
+	
+	mnozenieMacierzy(M, b, C, size);
+	
+	Temp_val = 1 - 1./w;
+	
+	mnozenieMacierzy(D, Temp_val, Temp, size);
+	dodawanieMacierzy(Temp, U, Temp1, size);
+	mnozenieMacierzy(M, Temp1, Temp, size);
+	mnozenieMacierzy(Temp, -1, M, size);
+	
+	bool endByOperations = 1;
+	
+	double estymator, residium;
+	
+	for(int i = 0; i < nOperations; i++){
+		mnozenieMacierzy(M, xm1, x, size);
+		dodawanieMacierzy(x, C, x, size);
+		
+		//Sprawdzanie, czy wielkoœæ kroku jet odpowiednia
+		estymator = maxKrokSize(x, xm1, size);
+		residium = maxDeviation(A, x, b, size);
+		
+		cout<<"Iteracja " << i << endl;
+		printVector(x, size);
+		cout<< endl << "Estymator: " << estymator << "\t\tResidium: " << residium << endl << endl;
+		
+		if(estymator < minStepSize){
+			endByOperations = 0;
+			cout << "Zakonczono, poniewaz krok byl zbyt maly!!" << endl;
+			break;
+		}
+		
+		if(residium < deviation){
+			endByOperations = 0;
+			cout << "Zakonczono, poniewaz znalezlismy sie zbyt blisko wyniku!!" << endl;
+			break;
+		}
+			
+		for(int j = 0; j < size; j++)
+			xm1[j] = x[j];
+	}
+	
+	if(endByOperations)
+		cout << "Zakonczono, poniewaz przekroczono ilosc operacji!!" << endl;
+		
+	deleting(M, size);
+	deleting(Dodwr, size);
+	deleting(Temp, size);
+	deleting(Temp1, size);
+	delete(xm1);
+	cout<< "???"<<endl;
+}
+
+
+
+int main(){
+	int n = 4;
+	
+	double **A = new double *[n];
+	allocating(A, n);
+	
+	double **L = new double *[n];
+	allocating(L, n);
+	
+	double **D = new double *[n];
+	allocating(D, n);
+	
+	double **U = new double *[n];
+	allocating(U, n);
+	
+	double **Dodw = new double *[n];
+	allocating(Dodw, n);
+	//Temp
+	
+	
+	double *b = new double [n];
+	double *x0 = new double [n];
+	double *x = new double [n];
+	
+	insertDefaultVal(A, b, x0);
+	podzialNaLDU(A, L, D, U, n);
+	
+	cout<< "A" << endl;
+	printMacierz(A, n);
+	cout<< "L" << endl;
+	printMacierz(L, n);
+	cout<< "D" << endl;
+	printMacierz(D, n);
+	cout<< "U" << endl;
+	printMacierz(U, n);
+	
+	macierzDiagonalnaOdwrotna(D, Dodw, n);
+	cout<< "D odwrotna" << endl;
+	printMacierz(Dodw, n);
+	
+	metodaJacobiego(A, L, D, U, x, x0, b, n);
+	
+	printVector(x, n);
+	
+	cout<< endl << endl << endl << "Metoda Gaussa -Seidela" << endl;
+	
+	metodaGaussaSeidela(A, L, U, D, x0, x, b, n);
+	
+	printVector(x, n);
+	
+	
+	cout<< endl << endl << endl << "Metoda SOR" << endl;
+	
+	metodaSOR(A, L, U, D, x0, x, b, 1/2., n);
+	
+	printVector(x, n);
+	
+	
+	deleting(A, n);
+	deleting(L, n);
+	deleting(D, n);
+	deleting(U, n);
+	deleting(Dodw, n);
+	delete(b);
+	delete(x0);
+	delete(x);
+
+	return 0;
+}
